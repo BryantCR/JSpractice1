f02df320c939b909afc4325da3817bea7a3352a6
BryantCR <bryancvargas20@gmail.com>
2021-09-10 13:26:17 -0600

    Nuew material to read

---
+++ main.js
@@ -0,0 +1,44 @@
+let fullName = "Bryan Cascante";
+var age = 19;
+let amIAInstructor = false;
+let hobby = null;
+let country = undefined;
+let state;
+
+console.log("Name: ", fullName);
+console.log("Age: ", age);
+console.log("Instructor: ", amIAInstructor);
+console.log("Hobby: ", hobby);
+console.log("Country: ", country);
+console.log("State: ", state);
+
+fullName = "Bryan Eduardo Cascante";
+
+console.log("Updated name: ", fullName)
+
+
+const PI = 3.1416;
+console.log("PI value: ", PI);
+
+/*
+    Operators
+        Aritmethic + - / * % ()
+        Relational
+*/  
+
+let result = (10 + 5) * 2 / 3;
+            // 15 * 2 / 3
+            // 30 / 3
+            // 10
+console.log( "Result: ", result );
+// 13.3 <----
+// 10
+
+/*
+    Order of operators
+    1. ()
+    2. * / %
+    3. + -
+    4. =
+
+*/
