5f51993802085455f17f85788ea2c3a7742f9e0f
BryantCR <bryancvargas20@gmail.com>
2021-09-16 11:42:16 -0600

    ok

---
+++ advancedFunctionsArrays.js
@@ -0,0 +1,10 @@
+let num = [10, 20, 30, 40, 50]
+
+//Get the number of elements inside the array
+console.log("Length", num.length);
+
+//Add an element at the end of the array
+num.push(60);
+console.log(num);
+
+//Add an element at the front of the array 
